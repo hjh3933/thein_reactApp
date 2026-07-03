634f84741c6438488ca9b4d9b02c9985efdd8024
Juhee <hkh3933@naver.com>
2026-07-03 17:03:49 +0900

    ch_hash

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="thein-reactApp">
+    <HashRouter basename="thein-reactApp">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
