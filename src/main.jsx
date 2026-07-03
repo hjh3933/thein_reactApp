1ce757701b707f45e2049fbe7b714955218cc9b9
Juhee <hkh3933@naver.com>
2026-07-03 16:19:06 +0900

    edit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
